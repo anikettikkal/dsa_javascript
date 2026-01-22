b16a0a1e5964970585f3d863a2204e22965cdddd
anikettikkal <tikkalaniket2019@gmail.com>
2026-01-22 11:57:13 +0530

    sum of two integer and relation between nteger and string

---
+++ basic.js
@@ -0,0 +1,15 @@
+// basics logic of js 
+// sum of two integer ans relation between integer and string
+
+// let a = 10
+// let b = "20"
+// console.log(typeof (a+b)); // string called concatenation
+
+// let a = 10
+// let b = 20
+// console.log("sum of a and b is" +a+b);
+// // output - sum of a and b is1020 bcoz in simple a+b +c so sabse pehle to sum of a and b concatenate hoga a ke sath fir concatenate hoga b ke sath
+// console.log("sum of a and b is " +(a+b)); // output is 30
+
+
+// type coercion
