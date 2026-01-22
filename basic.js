c0f64e716e41135326192d15cccada25092fcae0
anikettikkal <tikkalaniket2019@gmail.com>
2026-01-22 22:57:17 +0530

    opertor concept clear

--- basic.js
+++ basic.js
@@ -60,11 +60,63 @@
 
 // 3. swap using destructring
 
-let a =20;
-let b =39;
+// let a =20;
+// let b =39;
 
-[a,b] = [b,a];
-console.log(a,b);
+// [a,b] = [b,a];
+// console.log(a,b);
+
+
+
+// opertor and their questions = 
+// Arithmetic = +,-,*,/,%
+// let a =90;
+// let b=20 ;
+
+// console.log(a/b); // 4.5
+// console.log(Math.floor(a/b)); // 4
+// console.log(a%b); // 10
+// console.log(b%a); // 20
+
+// let a = 4563
+// let b,c ,d,e ;
+// b = a % 10 ; // 3
+// a = Math.floor(a/10);
+// c = a % 10 ; // 6
+// a = Math.floor(a/10);
+// d = a % 10 ; // 5
+// a = Math.floor(a/10);
+// e = a%10 ; // 4
+// a = Math.floor(a/10);
+
+// console.log(b+c+d+e);
+
+// relational operator <,>,<=,>=,!=
+
+// logical operator && ||
+
+// unary operator 
+// ++ increment -- decrement
+
+// let a =10;
+// let b = ++a; // post increment
+// console.log(a);
+// console.log(b);
+
+// let a= 11;
+// a = a++ + ++a ;
+// console.log(a);
+
+// let x = true;
+// x++
+// console.log(x); // output is 2
+
+// let z = 10
+// z = ++(z++)
+// console.log(z); // shows error bcoz z++ become 10 and ++10 is not valid
+
+
+
 
 
 
