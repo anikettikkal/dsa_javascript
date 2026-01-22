b2d2ec5e2390fd66e084972cb1088ee8554359d8
anikettikkal <tikkalaniket2019@gmail.com>
2026-01-22 12:23:13 +0530

    type casting and type coercion

--- basic.js
+++ basic.js
@@ -13,3 +13,23 @@
 
 
 // type coercion
+
+// console.log("1"+1); // 11
+// console.log("1"-1); // 0 why?
+// console.log("1"*1); // 1
+// console.log("1"/1); // 1
+
+// ans is + operator has two work i.e to add or to concatenate. other side * , -, / operator has only one work i.e substract , multiply or divide so compiler does that
+
+// get an value from user 
+
+const prompt = require("prompt-sync")(); // import this for get an values from user
+
+
+let age  = prompt("enter your age -") // typeof output is string so we convert it into number using Number()
+age = Number(age) // called type casting or type conversion
+console.log(age);
+
+
+
+
