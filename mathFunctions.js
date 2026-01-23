088d848ecf30d471e9886fba09bac02f6b9fdbc0
anikettikkal <tikkalaniket2019@gmail.com>
2026-01-23 12:11:02 +0530

    basics and math functions done

---
+++ mathFunctions.js
@@ -0,0 +1,49 @@
+// math functions
+// console.log(Math.round(10.5)); // 11
+// console.log(Math.ceil(10.1)); // 11
+// console.log(Math.floor(10.3)); // 10
+// console.log(Math.trunc(10.98)); // 10 - remove extra values next to point
+// console.log(Math.pow(2,5)); // 2^5 - 32
+// console.log(Math.sqrt(69)); // for square root of any number
+// console.log(Math.cbrt(2525)); // cube root of number
+// console.log(Math.abs(-28)); // convert negative values into postive
+// console.log(Math.max(24,5,67,545,444)); // find maximum number
+// console.log(Math.min(24,5,67,545,444)); // find min number
+// console.log(Math.floor(Math.random()*1000+1000)); // generate random number
+
+// let a = 837.3722942 ;
+// console.log(a.toFixed(2)); // output 837.37 and convert it into string
+// console.log(a.toLocaleString("en-IN")); // convert it into local language
+
+
+
+// Math Problems Solving
+// 1. Cal. area and Perimeter of rectangle
+// let l = 20;
+// let b = 40;
+// console.log(l*b);
+// console.log(2*(l*b));
+
+
+// area of tringle by herons formula - sqrt s *(s-a)*(s-b)*(s-c) & s - (a+b+c)/2
+// let a = 5 ;
+// let b = 10 ;
+// let c = 34 ;
+// let s = (a+b+c)/2 ;
+// console.log(s);
+// let area = Math.sqrt(Math.abs(s*(s-a)*(s-b)*(s-c))) ;
+// console.log(area);
+
+
+
+
+
+// 3. Circumferance of circle
+
+let r = 7;
+let c = 2 * Math.PI * r;
+console.log(c);
+
+
+
+
