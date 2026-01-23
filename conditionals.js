763c7ff8bdda1a1c4845bd934380f60ba2066d42
anikettikkal <tikkalaniket2019@gmail.com>
2026-01-23 22:58:57 +0530

    bijli amount

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,55 @@
 // valid voter
 const prompt = require("prompt-sync")();
 
-let age = Number(prompt("What is Your Age?"));
-if (isNaN(age)) {
-    console.log("wrong input");
-}
-else if (age>=18) {
-    console.log("You are Eligible For Voting");
-} else{
-    console.log("You are Not Eligible for Vote");   
-}
+// let age = Number(prompt("What is Your Age?"));
+// if (isNaN(age)) {
+//     console.log("wrong input");
+// }
+// else if (age>=18) {
+//     console.log("You are Eligible For Voting");
+// } else{
+//     console.log("You are Not Eligible for Vote");   
+// }
+
+// 2. Shop Discount
+// we do not find the discount we want to find the payble amount
+
+// let amount = Number(prompt("What is the final Amount?"));
+// let dis = 0;
+// if (amount<=2000) {
+//     dis =0;
+// } else if (amount>=2001 && amount<=4000) {
+//     dis=10;
+// } else if (amount>=4001 && amount<=9000) {
+//     dis=15;
+// } else if (amount>=10000) {
+//     dis=25;
+// } else{
+//     console.log("Wrong Input");
+// }
+
+// console.log(amount -(Math.floor(dis*amount)/100));
+
+// let unit = Number(prompt("Enter Electricity Unit")); // 700
+// let amount = 0;
+
+// if (unit>400) {
+//     amount = (unit-400)*13; //3900
+//     unit = 400
+// }
+// if (unit>200 && unit<=400) {
+//     amount += (unit-200)*8; // 1600
+//     unit =200 
+// }
+// if (unit>100 && unit<=200) {
+//     amount += (unit-100)*6; // 600
+//     unit = 100
+// }
+// amount += unit*4 ;
+// console.log(amount);
+
+
+
+
+// Inr denomination
+
